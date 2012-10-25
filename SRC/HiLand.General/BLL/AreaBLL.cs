3bd15e146a9d6798d0ad10f7062d3bb7ebe86e65
notinmood <notinmood@gmail.com>
2012-10-25 17:38:19 +0800

    解然修改信息

--- SRC/HiLand.General/BLL/AreaBLL.cs
+++ SRC/HiLand.General/BLL/AreaBLL.cs
@@ -14,6 +14,15 @@ namespace HiLand.General.BLL
     /// </summary>
     public class AreaBLL : BaseBLL<AreaBLL, AreaEntity, AreaDAL>
     {
-        
+        /// <summary>
+        /// 通过父级别的地区编码获取子地区列表
+        /// </summary>
+        /// <param name="parentCode">父级别的地区编码</param>
+        /// <returns></returns>
+        public List<AreaEntity> GetListByParentCode(string parentCode)
+        {
+            string whereClause = string.Format(" AreaCode like '{0}%' AND Len(AreaCode)={1} ",parentCode, parentCode.Length + 2);
+            return base.GetList(whereClause);
+        }
     }
 }
