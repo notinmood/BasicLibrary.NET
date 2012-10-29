95544315e0ad6aa5f5d1be096e0d85443e6b0f2e
notinmood <notinmood@gmail.com>
2012-10-29 17:54:39 +0800

    解然修改信息

--- SRC/HiLand.Utility/Office/ExcelHelper.cs
+++ SRC/HiLand.Utility/Office/ExcelHelper.cs
@@ -1,5 +1,9 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Reflection;
+using HiLand.Utility.Reflection;
 using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
 
@@ -64,6 +68,56 @@ namespace HiLand.Utility.Office
             workbook = null;
 
             return ms;
+        }
+
+        /// <summary>
+        /// 获取实体列表导出为Excel的流
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entityList"></param>
+        /// <param name="fieldsMap">实体的属性名称与Excel列名的映射字典</param>
+        /// <returns></returns>
+        public static Stream WriteExcel<T>(IList<T> entityList, Dictionary<string, string> fieldsMap)
+        {
+            DataTable dataTable = new DataTable();
+
+            Type entityType = typeof(T);
+            PropertyInfo[] propertyArray = entityType.GetProperties();
+
+            //1.创建表头
+            foreach (KeyValuePair<string, string> kvp in fieldsMap)
+            {
+                PropertyInfo piMatched = null;// propertyArray.First(s => s.Name == kvp.Key);
+                foreach (PropertyInfo piItem in propertyArray)
+                {
+                    if (piItem.Name == kvp.Key)
+                    {
+                        piMatched = piItem;
+                        break;
+                    }
+                }
+
+                if (piMatched != null)
+                {
+                    DataColumn dc = new DataColumn(kvp.Key, piMatched.PropertyType);
+                    dc.Caption = kvp.Value;
+                    dataTable.Columns.Add(dc);
+                }
+            }
+
+            //2.添加表数据
+            foreach (T item in entityList)
+            {
+                DataRow row = dataTable.NewRow();
+                foreach (KeyValuePair<string, string> kvp in fieldsMap)
+                {
+                    row[kvp.Key] = ReflectHelper.GetPropertyValue(item, kvp.Key);
+                }
+                dataTable.Rows.Add(row);
+            }
+
+            Stream outStream = ExcelHelper.WriteExcel(dataTable);
+            return outStream;
         }
 
         /// <summary>
