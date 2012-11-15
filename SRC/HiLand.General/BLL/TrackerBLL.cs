1b3c45650eb9862111ac546651ab992556f20df4
notinmood <notinmood@gmail.com>
2012-11-15 15:14:10 +0800

    添加回访、跟踪信息

---
+++ SRC/HiLand.General/BLL/TrackerBLL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using HiLand.Framework.BusinessCore;
+using HiLand.Framework.BusinessCore.BLL;
+using HiLand.Framework.FoundationLayer;
+using HiLand.General.DAL;
+using HiLand.General.Entity;
+using HiLand.Utility.Data;
+using HiLand.Utility.Enums;
+
+namespace HiLand.General.BLL
+{
+    /// <summary>
+    /// 回访、跟踪逻辑类
+    /// </summary>
+    public class TrackerBLL : BaseBLL<TrackerBLL, TrackerEntity, TrackerDAL>
+    {
+        
+    }
+}
---
+++ SRC/HiLand.General/DAL/TrackerDAL.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+using HiLand.General.DALCommon;
+
+namespace HiLand.General.DAL
+{
+    /// <summary>
+    /// 回访、跟踪数据访问类
+    /// </summary>
+    public class TrackerDAL : TrackerCommonDAL<SqlTransaction, SqlConnection, SqlCommand, SqlDataReader, SqlParameter>
+    {
+
+    }
+}
