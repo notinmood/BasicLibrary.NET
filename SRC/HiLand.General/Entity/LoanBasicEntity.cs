e1d93c3a9f1dba7ec13dd94432df6a9cc3df132f
notinmood <notinmood@gmail.com>
2013-01-08 16:09:38 +0800

    是的法定分

--- SRC/HiLand.General/Entity/LoanBasicEntity.cs
+++ SRC/HiLand.General/Entity/LoanBasicEntity.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using HiLand.Framework.BusinessCore;
+using HiLand.Framework.BusinessCore.BLL;
 using HiLand.Framework.FoundationLayer;
 using HiLand.Framework.FoundationLayer.Attributes;
 using HiLand.Utility.Data;
@@ -6,6 +9,9 @@ using HiLand.Utility.Finance;
 
 namespace HiLand.General.Entity
 {
+    /// <summary>
+    /// 贷款信息实体
+    /// </summary>
     public class LoanBasicEntity : BaseModel<LoanBasicEntity>
     {
         #region 实体信息
@@ -116,17 +122,80 @@ namespace HiLand.General.Entity
         }
 
         #region 扩展属性
+        private List<BusinessUser> usersInEnterprise = null;
+        /// <summary>
+        /// 当前贷款对应企业内的所有人员
+        /// </summary>
+        private List<BusinessUser> UsersInEnterprise
+        {
+            get
+            {
+                if (usersInEnterprise == null)
+                {
+                    usersInEnterprise = BusinessUserBLL.GetUsersByDepartment(this.LoanUserID);
+                }
+
+                return usersInEnterprise;
+            }
+        }
+
+        public BusinessUser loadUserReal = null;
+        /// <summary>
+        /// 贷款人信息（如果是企业用户，显示第一个企业负责人）
+        /// </summary>
+        public BusinessUser LoadUserReal
+        {
+            get
+            {
+                if (this.loadUserReal == null)
+                {
+                    if (usersInEnterprise == null || usersInEnterprise.Count == 0)
+                    {
+                        loadUserReal = BusinessUser.Empty;
+                    }
+                    else
+                    {
+                        loadUserReal = usersInEnterprise[0];
+                    }
+                }
+
+                return this.loadUserReal;
+            }
+        }
+
         private string loanUserRealName = string.Empty;
+        /// <summary>
+        /// 贷款人的名字（如果是企业用户，显示第一个企业负责人）
+        /// </summary>
         public string LoanUserRealName
         {
-            get { return loanUserRealName; }
+            get
+            {
+                if (string.IsNullOrEmpty(this.loanUserRealName))
+                {
+                    loanUserRealName = LoadUserReal.UserNameDisplay;
+                }
+
+                return loanUserRealName;
+            }
             set { loanUserRealName = value; }
         }
 
         private Guid loanUserRealID = Guid.Empty;
+        /// <summary>
+        /// 贷款人的Guid（如果是企业用户，显示第一个企业负责人）
+        /// </summary>
         public Guid LoanUserRealID
         {
-            get { return loanUserRealID; }
+            get
+            {
+                if (loanUserRealID == Guid.Empty)
+                {
+                    loanUserRealID = LoadUserReal.UserGuid;
+                }
+
+                return loanUserRealID;
+            }
             set { loanUserRealID = value; }
         }
         #endregion
