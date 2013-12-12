3983ff725cde4affa9f5c350dbd4ed4b0b4f0932
xieran <develope@live.com>
2013-12-12 15:39:45 +0800

    提交

---
+++ SRC/SampleConsoleWindowsForms/Tests/TextBox中Validating事件验证.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HiLand.Framework.WindowsFormsConsole.Tests
+{
+    public partial class TextBox中Validating事件验证 : Form
+    {
+        public TextBox中Validating事件验证()
+        {
+            InitializeComponent();
+        }
+    }
+}
