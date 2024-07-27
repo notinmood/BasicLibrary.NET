3fc0ebe832d54cbf217391f41ddd8f7bfa3005c9
解大劦 <develope@163.com>
2024-07-27 09:54:52 +0800

    misc

--- SRC/Hiland.BasicLibrary/Data/EnumerableEx.cs
+++ SRC/Hiland.BasicLibrary/Data/EnumerableEx.cs
@@ -1,6 +1,20 @@
 namespace Hiland.BasicLibrary.Data;
 
-public class EnumerableEx
+public static class EnumerableEx
 {
+     /// <summary>
+     /// 计算两个集合的笛卡尔积
+     /// </summary>
+     /// <typeparam name="T"></typeparam>
+     /// <typeparam name="P"></typeparam>
+     /// <param name="firstCollection"></param>
+     /// <param name="secondCollection"></param>
+     /// <returns>元组集合</returns>
+    public static IEnumerable<(T, P)> Product<T, P>(this IEnumerable<T> firstCollection, IEnumerable<P> secondCollection)
+    {
+        var result = firstCollection.SelectMany(s => secondCollection, (x, y) => (x, y));
+        return result;
+    }
+
 
 }
